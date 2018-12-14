936c89dd3b58f05e05e6f8e61ac55ef326e78e09
Catalin Pit <catalinpit@gmail.com>
2018-12-14 14:59:09 +0200

    .

--- MatchingSite/settings.py
+++ MatchingSite/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'MatchingSite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dc473qjt2t1dta',
-        'USER': 'exllgddigksknh',
-        'PASSWORD': 'eb3786dac300a38af0461eb28e20142e199a9bf9418e5ef78fc48fb9fe0cb3d8',
-        'HOST': 'ec2-54-243-238-46.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
