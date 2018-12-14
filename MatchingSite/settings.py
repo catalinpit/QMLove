d79b44fc7357bd84ca0f2cd397e560c6566cc70b
Catalin Pit <catalinpit@gmail.com>
2018-12-14 15:19:10 +0200

    jj

--- MatchingSite/settings.py
+++ MatchingSite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
